2cbcb551272da8f7bb89bba6751ac4f50845eb09
SalemNabeel <snabeel@elatechllc.com>
2021-05-23 10:36:50 +0300

    adding and initialize the spring-boot project

---
+++ src/test/java/com/bit/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
